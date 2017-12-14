9c502b540e07c9b360c110a68b395c94bb69d76c
Rudi Ridwan <ridwan28ixg@gmail.com>
2017-12-14 08:22:15 +0700

    Add files via upload

---
+++ BinaryTreeNode.java
@@ -0,0 +1,18 @@
+package tugas_bt_sorting;
+
+
+public class BinaryTreeNode {
+    Object element;
+    BinaryTreeNode leftChild;
+    BinaryTreeNode rightChild;
+    public BinaryTreeNode(){}
+    public BinaryTreeNode(Object theElement){
+        element = theElement;
+    }
+    public BinaryTreeNode(Object theElement,BinaryTreeNode theleftChild,BinaryTreeNode therightChild){
+        element = theElement;
+        leftChild = theleftChild;
+        rightChild = therightChild;
+    }
+    
+}
